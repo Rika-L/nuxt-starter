7fd7bee86f3f47e252427c84adffff7c9ea5a001
Lex <sx_lex@canway.net>
2025-05-13 15:43:06 +0800

    initial

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <UApp>
+    <NuxtRouteAnnouncer />
+    <UIcon
+      name="i-lucide-lightbulb"
+      class="size-5"
+    />
+
+    {{ aas }}
+  </UApp>
+</template>
+
+<script setup lang="ts">
+// sd
+const aas = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  modules: ['@nuxt/ui', '@nuxt/eslint'],
+  css: ['~/assets/css/main.css'],
+  ui: {
+    fonts :false
+  },
+  eslint: {
+    config: {
+      stylistic: true // <---
+    }
+  },
+  devtools: { enabled: true }
+})
